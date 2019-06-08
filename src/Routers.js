2f343f40cb9fd67efe047f1c34c23e500d743988
fc <327619852@qq.com>
2019-06-08 19:28:34 +0800

    feat(register) : init register

--- src/Routers.js
+++ src/Routers.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from './pages/login'
 import Home from './pages/home'
-import Follow from './pages/follow';
+import Follow from './pages/follow'
+import Register from './pages/register'
+import register from './pages/register';
 
 function Routers() {
   return (
     <Router>
       <Route exact path="/home/" component={Home} />
       <Route exact path="/" component={Login} />
+      <Route exact path="/register/" component={register} />
       <Route exact path="/follow/" component={Follow} />
     </Router>
   )
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,11 +2,12 @@ import { combineReducers } from 'redux'
 import { reducer as loginReducer } from '../pages/login/store'
 import { reducer as homeReducer } from '../pages/home/store'
 import { reducer as followReducer } from '../pages/follow/store'
-
+import {reducer as registerReducer } from '../pages/register/store'
 const reducer = combineReducers({
   login: loginReducer,
   home: homeReducer,
-  follow: followReducer
+  follow: followReducer,
+  register: registerReducer
 })
 
 export default reducer
