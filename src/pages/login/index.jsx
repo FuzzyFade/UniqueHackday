772a487bc98499f8367c5e86734dee59fde9d470
Zhenbin Wu <wzb3422@gmail.com>
2019-06-08 13:06:37 +0800

    :tada: feat(redux): connect redux

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
 class Login extends Component {
+
+
   render() {
     return (
       <div>{this.props.text}</div>
