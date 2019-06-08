d5d3775edf0f6caf9b2fb11611f7cd7992f04dc0
Zhenbin Wu <wzb3422@gmail.com>
2019-06-08 16:40:16 +0800

    :tada: feat(home): finish layout

---
+++ src/pages/home/components/ItemList/style.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const ItemListWarpper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`
+
+export const ItemBox = styled.div`
+  width: 320px;
+  height: auto;
+  border: 1px solid #eee;
+  border-radius: 5px;
+  box-shadow: 0 0 3px 3px #eee;
+  margin-bottom: 20px;
+`
+
+export const ItemHeader = styled.div`
+  width: 100%;
+  height: 30px;
+  line-height: 30px;
+  padding-left: 20px;
+`
+
+export const ItemContent = styled.div`
+  width: 100%;
+  height: auto;
+  padding: 0 10px 4px 20px;
+`
---
+++ src/pages/home/style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Warpper = styled.div`
+  .ant-tabs-nav-scroll {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`
